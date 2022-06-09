72a06134d7118c4069468de9c55876d64272e765
asmark031 <abhishek@assetplus.io>
2022-06-09 13:48:51 +0530

    added axios and one test API

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
+const cors = require('cors')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+// This is an example route which is been used in the frontend project for testing
+app.get('/test', (req, res) => {
+    res.json({
+        name: "Rakesh",
+        age: "34",
+        email: "rakesh.k@assetplus.io"
+    })
 })
 
 app.listen(port, () => {
